06bde01efe4145434aa97a3d2d118f3498fa020c
Rob Venn <rvenn.colab@gmail.com>
2025-01-30 16:43:54 +0000

    initial shop version
    Signed-off-by: Rob Venn <rvenn.colab@gmail.com>

---
+++ src/Driver.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class Driver{
+
+    private Scanner input = new Scanner(System.in);
+    private Product productArray;
+
+    public static void main(String[] args) {
+		Driver driver = new Driver();
+		driver.addProduct();
+		driver.printProduct();
+	}
+
+    //gather the product data from the user and create a new product object.
+    private void addProduct(){
+    	System.out.print("Enter the Product Name:  ");
+        String productName = input.nextLine();
+        System.out.print("Enter the Product Code:  ");
+        int productCode = input.nextInt();
+        System.out.print("Enter the Unit Cost:  ");
+        double unitCost = input.nextDouble();
+
+       //user input true or false on if the product is in line
+    	System.out.print("Is this product in your current line (y/n): ");
+    	char currentProduct = input.next().charAt(0);
+    	boolean inCurrentProductLine = false;
+    	if ((currentProduct == 'y') || (currentProduct == 'Y'))
+    		inCurrentProductLine = true;
+    	
+        productArray = new Product(productName, productCode, unitCost, inCurrentProductLine);
+    }
+    
+    //print the product calling toString method
+    private void printProduct(){
+    	System.out.println(productArray);
+    }
+	   	
+}
---
+++ src/Product.java
@@ -0,0 +1,61 @@
+
+public class Product {
+
+    //default values
+    private String productName = "";
+    private int productCode = -1;
+    private double unitCost = 0;
+    private boolean inCurrentProductLine = false;
+
+  //constructor class for product, name, code, cost and if its in product line or not.
+    public Product(String productName, int productCode, double unitCost, boolean inCurrentProductLine) {
+        this.productName = productName;
+        this.productCode = productCode;
+        this.unitCost = unitCost;
+        this.inCurrentProductLine = inCurrentProductLine;
+    }
+         
+ //getters to return product name, cost, code and if its in product line or not
+    public String getProductName(){
+        return productName;
+    }
+
+    public double getUnitCost(){
+        return unitCost;
+    }
+
+    public int getProductCode() {
+		return productCode;
+	}
+
+    public boolean isInCurrentProductLine() {
+		return inCurrentProductLine;
+	}
+
+//sets parameters based on user input
+	public void setProductCode(int productCode) {
+		this.productCode = productCode;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public void setUnitCost(double unitCost) {
+		this.unitCost = unitCost;
+	}
+
+	public void setInCurrentProductLine(boolean inCurrentProductLine) {
+		this.inCurrentProductLine = inCurrentProductLine;
+	}
+
+   //string built, displays user input data
+    public String toString()
+    {
+        return "Product description: " + productName
+             + ", product code: " + productCode
+             + ", unit cost: " + unitCost
+             + ", currently in product line: " + inCurrentProductLine;
+    }
+
+}
