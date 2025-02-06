336f28371c4194e093e711bd007d1f8021d6e5d1
Rob Venn <rvenn.colab@gmail.com>
2025-02-06 10:25:17 +0000

    Shop V3.0 refactoring for ArrayList, all classes Signed-off-by: Rob Venn <rvenn.colab@gmail.com>
    Signed-off-by: Rob Venn <rvenn.colab@gmail.com>

--- src/ScannerInput.java
+++ src/ScannerInput.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-
+//class gets rid of scanner bug by creating a new object for each user read,
+//also validates the data to ensure it is correct or doesn't accept it from the user
 public class ScannerInput {
-
+//user is prompted to enter an int which is then read by scanner class, must be an actual int
     public static int readNextInt(String prompt) {
         do {
             var scanner = new Scanner(System.in);
@@ -15,7 +16,7 @@ public class ScannerInput {
         }  while (true);
     }
 
-
+    //user is prompted to enter a double which is then read by scanner class, must be an actual double
     public static double readNextDouble(String prompt) {
         do {
             var scanner = new Scanner(System.in);
@@ -29,13 +30,13 @@ public class ScannerInput {
         }  while (true);
     }
 
-
+    //user is prompted to enter a string which is then read by scanner class, must be an actual string
     public static String readNextLine(String prompt) {
         Scanner input = new Scanner(System.in);
         System.out.print(prompt);
         return input.nextLine();
     }
-
+    //user is prompted to enter an char which is then read by scanner class, must be an actual char
     public static char readNextChar(String prompt) {
         Scanner input = new Scanner(System.in);
         System.out.print(prompt);
