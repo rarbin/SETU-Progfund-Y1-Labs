cc9cf2c73483dac3f022133740567d225afc07fc
Rob Venn <rvenn.colab@gmail.com>
2025-03-07 23:50:45 +0000

    Shop V6.0 *Add JUnit tests for classes Signed-off-by: Rob Venn <rvenn.colab@gmail.com>
    Signed-off-by: Rob Venn <rvenn.colab@gmail.com>

---
+++ test/controllers/StoreTest.java
@@ -0,0 +1,132 @@
+package controllers;
+
+import models.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreTest {
+
+    private Product productBelow, productExact, productAbove, productZero;
+    private Store storeWithProducts = new Store();
+    private Store storeEmpty = new Store();
+
+    @BeforeEach
+    void setUp() {
+        //name, 19 chars, code 999, unitCost 1, inCurrentProductLine true.
+        productBelow = new Product("Television 42Inches", 999, 1, true);
+        //name, 20 chars, code 1000, unitCost 999, inCurrentProductLine true.
+        productExact = new Product("Television 50 Inches", 1000, 999, true);
+        //name, 21 chars, code 10000, unitCost 1000, inCurrentProductLine false.
+        productAbove = new Product("Television 60 Inches.", 10000, 1000, false);
+        //name, 0 chars, code 9999, unitCost 0, inCurrentProductLine false.
+        productZero = new Product("", 9999, 0, false);
+
+        storeWithProducts.add(productBelow);
+        storeWithProducts.add(productExact);
+        storeWithProducts.add(productAbove);
+        storeWithProducts.add(productZero);
+    }
+
+    @AfterEach
+    void tearDown() {
+        productBelow = productExact = productAbove = productZero = null;
+        storeEmpty = storeWithProducts = null;
+    }
+
+    @Nested
+    class CRUD {
+        @Test
+        void addingToAnArrayListThatHasProductsIsSuccessful() {
+            assertEquals(4, storeWithProducts.numberOfProducts());
+            assertTrue(storeWithProducts.add(productZero));
+            assertEquals(5, storeWithProducts.numberOfProducts());
+            assertEquals(productZero, storeWithProducts.findProduct(4));
+        }
+
+        @Test
+        void addingToAnArrayListThatHasNoProductsIsSuccessful() {
+            assertEquals(0, storeEmpty.numberOfProducts());
+            assertTrue(storeEmpty.add(productZero));
+            assertEquals(1, storeEmpty.numberOfProducts());
+            assertEquals(productZero, storeEmpty.findProduct(0));
+        }
+
+
+        @Test
+        void listProductsReturnsProductsWhenArrayListHasProductsStored() {
+            assertEquals(4, storeWithProducts.numberOfProducts());
+            String productsString = storeWithProducts.listProducts();
+            assertTrue(productsString.contains("Television 42Inches"));
+            assertTrue(productsString.contains("Television 50 Inches"));
+            assertTrue(productsString.contains("Television 60 Inches"));
+            assertTrue(productsString.contains(""));
+        }
+
+        @Test
+        void listProductsReturnsNoProductsStoredWhenArrayListIsEmpty() {
+            assertEquals(0, storeEmpty.numberOfProducts());
+            assertTrue(storeEmpty.listProducts().toLowerCase().contains("no products"));
+        }
+
+        @Test
+        void deletingAProductThatExistsDeletesAndReturnsDeletedObject() {
+            assertEquals(4, storeWithProducts.numberOfProducts());
+            assertEquals(productAbove, storeWithProducts.deleteProduct(2));
+            assertEquals(3, storeWithProducts.numberOfProducts());
+        }
+
+        @Test
+        void deletingAProductThatDoesNotExistReturnsNull() {
+            assertNull(storeEmpty.deleteProduct(0));
+            assertNull(storeWithProducts.deleteProduct(-1));
+            assertNull(storeWithProducts.deleteProduct(4));
+        }
+
+        @Test
+        void updatingANoteThatExistsReturnsTrueAndUpdates() {
+            //check product index 2 exists and check the contents
+            assertEquals(productAbove, storeWithProducts.findProduct(2));
+            assertEquals("Television 60 Inches", storeWithProducts.findProduct(2).getProductName());
+            assertEquals(-1, storeWithProducts.findProduct(2).getProductCode());
+            assertEquals(1000, storeWithProducts.findProduct(2).getUnitCost());
+            assertFalse(storeWithProducts.findProduct(2).isInCurrentProductLine());
+
+            //update product 2 with new information and ensure contents updated successfully
+            assertTrue(storeWithProducts.updateProduct(2, "Updating Product", 2000, 19.99, true));
+            assertEquals("Updating Product", storeWithProducts.findProduct(2).getProductName());
+            assertEquals(2000, storeWithProducts.findProduct(2).getProductCode());
+            assertEquals(19.99, storeWithProducts.findProduct(2).getUnitCost());
+            assertTrue(storeWithProducts.findProduct(2).isInCurrentProductLine());
+        }
+
+        @Test
+        void updatingAProductThatDoesNotExistReturnsFalse() {
+            assertFalse(storeWithProducts.updateProduct(4, "Updating Product", 2, 19.99, false));
+            assertFalse(storeWithProducts.updateProduct(-1, "Updating Product", 1002, 14.49, true));
+            assertFalse(storeEmpty.updateProduct(0, "Updating Product", 1003, 199.99, false));
+        }
+    }
+@Nested
+class FindAndSearch {
+    @Test
+    void findProductReturnsProductWhenIndexIsValid() {
+        assertEquals(4, storeWithProducts.numberOfProducts());
+        assertEquals(productBelow, storeWithProducts.findProduct(0));
+        assertEquals(productAbove, storeWithProducts.findProduct(2));
+    }
+
+    @Test
+    void findProductReturnsNullWhenIndexIsInValid() {
+        assertEquals(0, storeEmpty.numberOfProducts());
+        assertNull(storeEmpty.findProduct(0));
+        assertEquals(4, storeWithProducts.numberOfProducts());
+        assertNull(storeWithProducts.findProduct(-1));
+        assertNull(storeWithProducts.findProduct(4));
+    }
+}
+}
---
+++ test/models/ProductTest.java
@@ -0,0 +1,127 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    private Product productBelow, productExact, productAbove, productZero;
+
+    @BeforeEach
+    void setUp() {
+        //name, 19 chars, code 999, unitCost 1, inCurrentProductLine true.
+        productBelow = new Product("Television 42Inches", 999, 1, true);
+        //name, 20 chars, code 1000, unitCost 999, inCurrentProductLine true.
+        productExact = new Product("Television 50 Inches", 1000, 999, true);
+        //name, 21 chars, code 10000, unitCost 1000, inCurrentProductLine false.
+        productAbove = new Product("Television 60 Inches.", 10000, 1000, false);
+        //name, 0 chars, code 9999, unitCost 0, inCurrentProductLine false.
+        productZero = new Product("", 9999, 0, false);
+    }
+
+    @AfterEach
+    void tearDown() {
+        productBelow = productExact = productAbove = productZero = null;
+    }
+
+    @Nested
+    class Getters {
+        @Test
+        void getProductName() {
+            assertEquals("Television 42Inches", productBelow.getProductName());
+            assertEquals("Television 50 Inches", productExact.getProductName());
+            assertEquals("Television 60 Inches", productAbove.getProductName());
+            assertEquals("", productZero.getProductName());
+        }
+
+        @Test
+        void getUnitCost() {
+            assertEquals(1, productBelow.getUnitCost());
+            assertEquals(999, productExact.getUnitCost());
+            assertEquals(1000, productAbove.getUnitCost());
+            assertEquals(0, productZero.getUnitCost());
+        }
+
+        @Test
+        void getProductCode() {
+            assertEquals(-1, productBelow.getProductCode());
+            assertEquals(1000, productExact.getProductCode());
+            assertEquals(-1, productAbove.getProductCode());
+            assertEquals(9999, productZero.getProductCode());
+        }
+
+        @Test
+        void isInCurrentProductLine() {
+            assertTrue(productBelow.isInCurrentProductLine());
+            assertTrue(productExact.isInCurrentProductLine());
+            assertFalse(productAbove.isInCurrentProductLine());
+            assertFalse(productZero.isInCurrentProductLine());
+        }
+    }
+
+    @Nested
+    class Setters {
+        @Test
+        void setProductName() {
+            assertEquals("Television 42Inches", productBelow.getProductName());
+
+            productBelow.setProductName("iPhone 13 Charcoal.");   //19 chars - update performed
+            assertEquals("iPhone 13 Charcoal.", productBelow.getProductName());
+
+            productBelow.setProductName("iPhone 12 - Charcoal");   //20 chars - update performed
+            assertEquals("iPhone 12 - Charcoal", productBelow.getProductName());
+
+            productBelow.setProductName("iPhone 11: - Charcoal");   //21 chars - update ignored
+            assertEquals("iPhone 12 - Charcoal", productBelow.getProductName());
+        }
+
+        @Test
+        void setUnitCost() {
+            assertEquals(999, productExact.getUnitCost());
+            productExact.setUnitCost(99.99);  //no validation performed
+            assertEquals(99.99, productExact.getUnitCost());
+        }
+
+        @Test
+        void setProductCode() {
+            assertEquals(-1, productAbove.getProductCode());
+
+            productAbove.setProductCode(999);  //update ignored
+            assertEquals(-1, productAbove.getProductCode());
+
+            productAbove.setProductCode(1000); //update performed
+            assertEquals(1000, productAbove.getProductCode());
+
+            productAbove.setProductCode(9999);  //update performed
+            assertEquals(9999, productAbove.getProductCode());
+
+            productAbove.setProductCode(10000);  //update ignored
+            assertEquals(9999, productAbove.getProductCode());
+        }
+
+        @Test
+        void setInCurrentProductLine() {
+            assertTrue(productExact.isInCurrentProductLine());
+            productExact.setInCurrentProductLine(false); //no validation performed
+            assertFalse(productExact.isInCurrentProductLine());
+
+            assertFalse(productAbove.isInCurrentProductLine());
+            productAbove.setInCurrentProductLine(true); //no validation performed
+            assertTrue(productAbove.isInCurrentProductLine());
+        }
+
+    }
+
+    @Test
+    void testToString() {
+        String toStringContents = productExact.toString();
+        assertTrue(toStringContents.contains("Product description: " + productExact.getProductName()));
+        assertTrue(toStringContents.contains("product code: " + productExact.getProductCode()));
+        assertTrue(toStringContents.contains("unit cost: " + productExact.getUnitCost()));
+        assertTrue(toStringContents.contains(" currently in product line: Y"));
+    }
+}
